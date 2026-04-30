dc31df941d73bb10c1517a2fb680d7914e218976
IMANHAZ <miimininiim@gmail.com>
2026-04-30 08:48:09 +0330

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,22 @@
+export const config = {
+runtime: 'edge',
+};
+‌
+export default async function handler(req) {
+const upstream = 'https://Faz.jojeyenaz.ir:8040';
+const url = new URL(req.url);
+const targetUrl = upstream + url.pathname + url.search;
+‌
+const newRequest = new Request(targetUrl, {
+method: req.method,
+headers: req.headers,
+body: req.body,
+redirect: 'follow'
+});
+‌
+try {
+return await fetch(newRequest);
+} catch (e) {
+return new Response('Error: ' + e.message, { status: 500 });
+}
+}
